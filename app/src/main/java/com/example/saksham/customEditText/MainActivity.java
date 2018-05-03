2547411993be67a4e4ad9d837aa3e6645b64276f
miPlodder <academic.rahul@gmail.com>
2018-05-03 15:53:51 +0530

    Migration from App to Library

--- app/src/main/java/com/example/saksham/customEditText/MainActivity.java
+++ app/src/main/java/com/example/saksham/customEditText/MainActivity.java
@@ -3,20 +3,16 @@ package com.example.saksham.customEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
-import android.text.Editable;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //functionality for space after 'n' characters
     //functionality for limiting to 'n' characters
-    BetterVisualizerEditText betterVisualizerEditText;
+    //BetterVisualizerEditText betterVisualizerEditText;
 
     //functionality for limiting 'n' integer places
     //functionality for limiting 'n' decimal places
-    DynamicAmountEditText dynamicAmountEditText;
+    //DynamicAmountEditText dynamicAmountEditText;
 
 
     AppCompatButton btnDone;
@@ -26,15 +22,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //betterVisualizerEditText = findViewById(R.id.bvet);
-        dynamicAmountEditText = findViewById(R.id.da_et);
-        btnDone = findViewById(R.id.btnDone);
-
-        btnDone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, dynamicAmountEditText.getActualText(), Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
--- library/src/main/java/com/example/library/DynamicAmountEditText.java
+++ library/src/main/java/com/example/library/DynamicAmountEditText.java
@@ -1,4 +1,4 @@
-package com.example.saksham.customEditText;
+package com.example.library;
 
 import android.content.Context;
 import android.content.res.TypedArray;
